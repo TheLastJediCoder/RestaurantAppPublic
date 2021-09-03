8a4370fdc65000bba58ef430bcc73afb1a439d86
Harsh Parecha <harshparecha@gmail.com>
2021-09-03 10:09:27 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -3,10 +3,8 @@ from flask_session import Session
 from views.view_admin_home import admin_home
 from views.view_menu import menu_home
 from views.view_deal import deal_home
-from views.view_team import team_home
 from views.view_menu_item import menu_item_home
 from views.view_deal_item import deal_item_home
-from views.view_team_member import team_member_home
 from views.view_home import home
 from models import *
 
@@ -23,8 +21,6 @@ app.register_blueprint(menu_home, url_prefix='/admin')
 app.register_blueprint(menu_item_home, url_prefix='/admin')
 app.register_blueprint(deal_home, url_prefix='/admin')
 app.register_blueprint(deal_item_home, url_prefix='/admin')
-app.register_blueprint(team_home, url_prefix='/admin')
-app.register_blueprint(team_member_home, url_prefix='/admin')
 
 
 if __name__ == '__main__':
--- setting.py
+++ setting.py
@@ -1,20 +1,15 @@
 import os
 import cloudinary.uploader
 
-# os.environ.get('')
 
-SQLALCHEMY_DATABASE_URI = 'MYSQL URI'
+SQLALCHEMY_DATABASE_URI = os.environ.get('MYSQL_URI')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SESSION_TYPE = 'sqlalchemy'
 SECRET_KEY = 'SECRET KEY'
 
-# cloudinary.config(
-#     cloud_name=os.environ.get('CLOUD_NAME'),
-#     api_key=os.environ.get('API_KEY'),
-#     api_secret=os.environ.get('API_SECRET')
-# )
 cloudinary.config(
-    cloud_name='cloud_name',
-    api_key='api_key',
-    api_secret='api_secret'
+    cloud_name=os.environ.get('CLOUD_NAME'),
+    api_key=os.environ.get('API_KEY'),
+    api_secret=os.environ.get('API_SECRET')
 )
+
